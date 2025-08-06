69f6bb39c797a78d8c070c3409b6cf6bcb96e6ec
jack_decibel <82787353+jcolicchio-soundag@users.noreply.github.com>
2025-08-06 13:08:48 -0700

    Update Sound_DMR.R

--- R/Sound_DMR.R
+++ R/Sound_DMR.R
@@ -1201,7 +1201,7 @@ sound_score <- function(changepoint_OF = dataframe, Statistic="Z_GroupT_small",
   #Calculate Sound Statistic
   RegionStats$dmr_score<-(((RegionStats$Count)^(1/3))*(abs(RegionStats[[MethRegion_Z]])*abs(RegionStats[[Per_Change]]))^(1/2))
   RegionStats$dmr_score2<-(((RegionStats$Count)^(1/3))*(abs(RegionStats[[MethRegion_Z]])*abs(asin(sqrt(RegionStats[[Per_Change]]/100+RegionStats[[Control]]/100))-asin(sqrt(RegionStats[[Control]]/100)))^(1/2)))
-  RegionStats$dmr_score_noZ<-(((RegionStats$Count)^(1/3))*abs(RegionStats[[Per_Change]]))^(1/2))
+  RegionStats$dmr_score_noZ<-(((RegionStats$Count)^(1/3))*abs(RegionStats[[Per_Change]])^(1/2))
   # if(CF==TRUE){
   #   RegionStats$CustomFunction<<-UserFunction
   #   RegionStats$CustomFunction_Percentile<-ecdf(RegionStats$CustomFunction)(RegionStats$CustomFunction)
